072970e5961fb04729b38c5d58e5d887bcd466d4
sp978077 <sp978077@gmail.com>
2023-04-25 17:08:13 +0530

    .

--- product/JwtDbApi/Models/Vendor.cs
+++ product/JwtDbApi/Models/Vendor.cs
@@ -7,7 +7,7 @@ namespace JwtDbApi.Models
         [Key]
         public int Id { get; set; }
         public string GSTIN { get; set; }
-        public int PinCode { get; set; }
+        public int DeliveryPinCode { get; set; }
         public int UserId { get; set; }
         //Relationships
         public List<ProductVendor> ProductVendors { get; set; }
