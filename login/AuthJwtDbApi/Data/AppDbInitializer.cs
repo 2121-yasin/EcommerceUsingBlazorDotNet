16ee1c0412fef78335171f3524398c6edf6ed15b
sp978077 <sp978077@gmail.com>
2023-04-26 13:46:47 +0530

    "modified db initializer"

--- login/AuthJwtDbApi/Data/AppDbInitializer.cs
+++ login/AuthJwtDbApi/Data/AppDbInitializer.cs
@@ -16,53 +16,13 @@ namespace AuthJwtDbApi.Data
 
                 context.Database.EnsureCreated();
 
-                if (!context.UserInfo.Any())
-                {
-                    context.UserInfo.AddRange(new List<UserInfo>()
-                    {
-                        new UserInfo()
-                        {
-                            UserId=1,
-                            UserName="Aaron",
-                            Email="aaron@gmail.com",
-                            Password= BCrypt.Net.BCrypt.HashPassword("Aaron@123"),
-                            Role="Admin",
-                            AddressId=1
-                        },
-                        new UserInfo()
-                        {
-                            UserId=2,
-                            UserName="Suraj",
-                            Email="suraj@gmail.com",
-                            Password= BCrypt.Net.BCrypt.HashPassword("Suraj@123"),
-                            Role="Vendor",
-                            AddressId=1
-
-                        },
-                        new UserInfo()
-                        {
-                            UserId=3,
-                            UserName="Yasin",
-                            Email="yasin@gmail.com",
-                            Password= BCrypt.Net.BCrypt.HashPassword("Yasin@123"),
-                            Role="User",
-                            AddressId=1
-
-                        }
-                    });
-                    context.SaveChanges();
-                }
-
-
-
-
                 if (!context.AddressInfo.Any())
                 {
                     context.AddressInfo.AddRange(new List<AddressInfo>()
                     {
                         new AddressInfo()
                         {
-                            AddressId=1,
+
                             Street="abc",
                             City="panaji",
                             State="Goa",
@@ -71,7 +31,7 @@ namespace AuthJwtDbApi.Data
                         },
                         new AddressInfo()
                         {
-                            AddressId=2,
+
                             Street="abc",
                             City="panaji",
                             State="Goa",
@@ -79,7 +39,7 @@ namespace AuthJwtDbApi.Data
                         },
                         new AddressInfo()
                         {
-                            AddressId=3,
+
                             Street="abc",
                             City="panaji",
                             State="Goa",
@@ -89,6 +49,49 @@ namespace AuthJwtDbApi.Data
                     });
                     context.SaveChanges();
                 }
+                if (!context.UserInfo.Any())
+                {
+                    context.UserInfo.AddRange(new List<UserInfo>()
+                    {
+                        new UserInfo()
+                        {
+
+                            UserName="Aaron",
+                            Email="aaron@gmail.com",
+                            Phone="123",
+                            Password= BCrypt.Net.BCrypt.HashPassword("Aaron@123"),
+                            Role="Admin",
+                            AddressId=1
+                        },
+                        new UserInfo()
+                        {
+
+                            UserName="Suraj",
+                            Email="suraj@gmail.com",
+                            Phone="123",
+                            Password= BCrypt.Net.BCrypt.HashPassword("Suraj@123"),
+                            Role="Vendor",
+                            AddressId=2
+
+                        },
+                        new UserInfo()
+                        {
+
+                            UserName="Yasin",
+                            Email="yasin@gmail.com",
+                            Phone="123",
+                            Password= BCrypt.Net.BCrypt.HashPassword("Yasin@123"),
+                            Role="User",
+                            AddressId=3
+
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+
+
+
 
 
 
@@ -100,7 +103,7 @@ namespace AuthJwtDbApi.Data
                     {
                         new ClientProfile()
                         {
-                            Id=1,
+
                             Name="Admin",
                             ClientId = new Guid("b6782e13-5669-4156-82a8-1850883214e4"),
                             RedirectUrl="http://localhost:3000"
@@ -108,7 +111,7 @@ namespace AuthJwtDbApi.Data
                         },
                         new ClientProfile()
                         {
-                            Id=2,
+
                             Name="Vendor",
                             ClientId = new Guid("ff84a00f-99ab-4f81-9f52-26df485a9dcf"),
                             RedirectUrl="http://localhost:3002"
@@ -116,7 +119,7 @@ namespace AuthJwtDbApi.Data
                         },
                         new ClientProfile()
                         {
-                            Id=3,
+
                             Name="User",
                             ClientId = new Guid("A0D0B3A2-EFA4-47CA-B193-45BDBD950F3A"),
                             RedirectUrl="https://localhost:7042"
