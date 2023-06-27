c5a3b4c2b36f441ff20a0345c63066fd9ad470f1
aarondesousa <aarondesousa1@gmail.com>
2023-06-27 11:28:39 +0530

    .

--- login/AuthJwtDbApi/DTOs/UserLoginDto.cs
+++ login/AuthJwtDbApi/DTOs/UserLoginDto.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-
 namespace AuthJwtDbApi.DTOs
 {
     public class UserLoginDto
@@ -13,7 +5,5 @@ namespace AuthJwtDbApi.DTOs
         //public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
     }
-
 }
--- login/AuthJwtDbApi/Models/AddressInfo.cs
+++ login/AuthJwtDbApi/Models/AddressInfo.cs
@@ -4,15 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace AuthJwtDbApi.Models
 {
     public class AddressInfo
-{
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int AddressId { get; set; }
-    public string Street { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public string Pincode { get; set; }
-    
-}
-
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Pincode { get; set; }
+    }
 }
